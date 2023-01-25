d759d3ac7ed6fc963990281fb530f9807d2ab61e
samuelgreorovic <samuelgregorovic@gmail.com>
2023-01-25 18:23:37 +0100

    rename to callpyback

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="callpyback",
+    packages=["callpyback"],
+    version="v0.0.1",  # Ideally should be same as your GitHub release tag varsion
+    description="Simple and readable Pure Python callbacks!",
+    author="samuelgregorovic",
+    author_email="samuelgregorovic@gmail.com",
+    url="https://github.com/samuelgregorovic/callpyback",
+    download_url="https://github.com/samuelgregorovic/callpyback/archive/refs/tags/v0.0.1.tar.gz",
+    keywords=["callpyback", "callback", "python", "pure", "pythonic"],
+    classifiers=[],
+)
