8a86220dc887dbebf8a040cd6beb0b0619dbd67f
samuelgreorovic <samuelgregorovic@gmail.com>
2023-01-29 19:00:25 +0100

    simple docstrings

--- callpyback/classes.py
+++ callpyback/classes.py
@@ -2,6 +2,7 @@ import sys
 import inspect
 import asyncio
 
+# default callback functions
 DEFAULT_ON_CALL_LAMBDA = lambda *a, **k: None
 DEFAULT_ON_SUCCESS_LAMBDA = lambda *a, **k: None
 DEFAULT_ON_FAILURE_LAMBDA = lambda *a, **l: None
@@ -9,6 +10,7 @@ DEFAULT_ON_END_LAMBDA = lambda *a, **k: None
 
 
 class CallPyBack:
+    # decorator init
     def __init__(
         self,
         on_call=DEFAULT_ON_CALL_LAMBDA,
@@ -16,7 +18,7 @@ class CallPyBack:
         on_failure=DEFAULT_ON_FAILURE_LAMBDA,
         on_end=DEFAULT_ON_END_LAMBDA,
         default_return=None,
-        pass_vars=[],
+        pass_vars=None,
     ):
         self.on_call = on_call
         self.on_success = on_success
@@ -27,11 +29,14 @@ class CallPyBack:
         self.local_vars = {}
 
     def __call__(self, func):
+        # call of the decorated function
         def tracer(frame, event, arg):
+            # setup for pulling locals
             if event == "return":
                 self.local_vars = frame.f_locals.copy()
 
         def wrapper(*func_args, **func_kwargs):
+            # callback and func flow
             sys.setprofile(tracer)
             func_exception, func_result, func_scope_vars = None, None, []
             try:
@@ -57,7 +62,10 @@ class CallPyBack:
 
         return wrapper
 
+    ############### CALLING CALLBACKS ###############
+
     def run_callback_func(self, func, func_kwargs):
+        # generalised call to given callback func
         if inspect.iscoroutinefunction(self.on_call):
             asyncio.run(func(**func_kwargs))
         else:
@@ -87,21 +95,31 @@ class CallPyBack:
         )
         self.run_callback_func(self.on_end, on_end_kwargs)
 
+    ############### END CALLING CALLBACKS ###############
+
     def get_func_scope_vars(self):
+        # getting local variables from func scope based on pass_vars param
         func_scope_vars = {}
+        if not self.pass_vars:
+            return []
         for var_name in self.pass_vars:
             func_scope_vars[var_name] = self.local_vars.get(var_name, "<not-found>")
         self.local_vars = {}
         return func_scope_vars
 
     def is_on_end_func_defined(self):
+        # check if on_end handling function is defined
         return (
             isinstance(self.on_end, type(DEFAULT_ON_END_LAMBDA))
             and self.on_end.__name__ == DEFAULT_ON_END_LAMBDA.__name__
             and self.on_end == DEFAULT_ON_END_LAMBDA
         )
 
+    ############### CONSTRUCTING KWARGS FOR CALLBACKS ###############
+    # to allow omitting parameters in user created callbacks
+
     def get_on_call_kwargs(self, func_args, func_kwargs):
+        # constructing on_call kwargs
         kwargs = {}
         params = inspect.signature(self.on_success).parameters
         if "func_args" in params:
@@ -111,6 +129,7 @@ class CallPyBack:
         return kwargs
 
     def get_on_success_kwargs(self, func_result, func_args, func_kwargs):
+        # constructing on_success kwargs
         kwargs = {}
         params = inspect.signature(self.on_success).parameters
         if "func_result" in params:
@@ -122,6 +141,7 @@ class CallPyBack:
         return kwargs
 
     def get_on_failure_kwargs(self, func_exception, func_args, func_kwargs):
+        # constructing on_failure kwargs
         kwargs = {}
         params = inspect.signature(self.on_failure).parameters
         if "func_exception" in params:
@@ -135,6 +155,7 @@ class CallPyBack:
     def get_on_end_kwargs(
         self, func_result, func_exception, func_args, func_kwargs, func_scope_vars
     ):
+        # constructing on_end kwargs
         kwargs = {}
         params = inspect.signature(self.on_end).parameters
         if "func_result" in params:
