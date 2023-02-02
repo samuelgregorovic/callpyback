6e74504f914c720692a86be45aded3b167689f24
samuelgreorovic <samuelgregorovic@gmail.com>
2023-02-02 20:15:30 +0100

    release v1.0.0 to pypi

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 setup_args = dict(
     name="callpyback",
     packages=find_packages(),
-    version="v0.0.6",
+    version="v1.0.0",
     description="Simple and readable Pure Python callbacks!",
     author="samuelgregorovic",
     author_email="samuelgregorovic@gmail.com",
     url="https://github.com/samuelgregorovic/callpyback",
-    download_url="https://github.com/samuelgregorovic/callpyback/archive/refs/tags/v.0.0.6.tar.gz",
+    download_url="https://github.com/samuelgregorovic/callpyback/archive/refs/tags/v.1.0.0.tar.gz",
     keywords=["callpyback", "callback", "python", "pure", "pythonic"],
     classifiers=[],
 )
@@ -18,3 +18,6 @@ install_requires = []
 
 if __name__ == "__main__":
     setup(**setup_args, install_requires=install_requires)
+
+# $ python setup.py sdist
+# $ twine upload dist/callpyback-v1.0.0.tar.gz
