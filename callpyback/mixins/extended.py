7d14152e400ca59443521fd306581a7c178d021a
samuelgreorovic <samuelgregorovic@gmail.com>
2023-02-07 18:02:34 +0100

    added support for passing func to callbacks + unit tests

--- callpyback/mixins/extended.py
+++ callpyback/mixins/extended.py
@@ -15,11 +15,12 @@ class ExtendedCallBackMixin:
         validate_exception_classes():
             Validates `exception_classes` constructor argument.
         get_func_scope_vars():
-            Gets requested decorated function's scope variables, specified in `pass_vars` attribute.
+            Gets requested decorated function's scope variables, specified in
+            `pass_vars` attribute.
         set_tracer_profile(tracer):
             Sets custom tracer profile.
         tracer(frame, event, _):
-            Represents tracer for storing local variables from last executed function.
+            Represents tracer storing local variables from executed function.
     """
 
     def __init__(
@@ -32,10 +33,10 @@ class ExtendedCallBackMixin:
         """Class constructor. Sets instance variables.
 
         Args:
-            default_return (Any, optional): Result to be returned in case of error or no return.
-                Defaults to None.
-            pass_vars (list|tuple|set, optional): Variable names to be passed to `on_end` callback.
-                Defaults to None.
+            default_return (Any, optional): Result to be returned in case
+                of error or no return. Defaults to None.
+            pass_vars (list|tuple|set, optional): Variable names to be passed
+                to `on_end` callback. Defaults to None.
             exception_classes (list|tuple|set): Exception classes to be caught.
                 Defaults to (Exception,).
 
@@ -62,52 +63,53 @@ class ExtendedCallBackMixin:
         Returns:
             None
         Raises:
-            TypeError: Raised if `pass_vars` is not of type `list`, `tuple` or `set`.
-            TypeError: Raised if one of variables in `pass_vars` is not of type `str`.
+            TypeError: Raised if `pass_vars` is not `list`, `tuple` or `set`.
+            TypeError: Raised if one of `pass_vars` elements is not `str`.
         """
         if not self.pass_vars:
             return
         if not isinstance(self.pass_vars, (list, tuple, set)):
-            raise TypeError("Parameter `pass_vars` must be `list`, `tuple` or `set`.")
+            raise TypeError("`pass_vars` must be `list`, `tuple` or `set`.")
         for var in self.pass_vars:
             if not isinstance(var, str):
-                raise TypeError("Variable in `pass_vars` must be of type `str`.")
+                raise TypeError("`pass_vars` elements must be of type `str`.")
 
     def validate_exception_classes(self):
         """Validates `exception_classes` constructor argument.
 
         Executes following checks:
-            1. Argument `exception_classes` must be of type `tuple`, `list` or `set`.
-            2. All elements of `exception_classes` must be subclasses of `Exception`.
+            1. Argument `exception_classes` must be `tuple`, `list` or `set`.
+            2. Elements of `exception_classes` must subclass of `Exception`.
 
         Args:
             N/A
         Returns:
             None
         Raises:
-            TypeError: Raised if `exception_classes` is not of type `tuple`, `list` or `set`
-            TypeError: Raised if any element of `exception_classes` is not subclass of `Exception`.
+            TypeError: `exception_classes` is not `tuple`, `list` or `set`
+            TypeError: Raised if any element of `exception_classes` is not
+                subclass of `Exception`.
         """
         if not isinstance(self.exception_classes, (list, tuple, set)):
-            raise TypeError(
-                "Parameter `exception_classes` must be `list`, `tuple` or `set`."
-            )
+            raise TypeError("`exception_classes` not `list`, `tuple` or `set`")
         for exception_class in self.exception_classes:
             if not inspect.isclass(exception_class):
-                raise TypeError("Element of `exception_classes` must be a class.")
+                raise TypeError("Element of `exception_classes` not a class.")
             if not issubclass(exception_class, Exception):
                 raise TypeError(
-                    "Element of `exception_classes` must be a subclass of `Exception`."
+                    "`exception_classes` must be a subclass of `Exception`."
                 )
 
     def get_func_scope_vars(self):
-        """Gets requested decorated function's scope variables, specified in `pass_vars` attribute.
-        Any missing requested scope variables will be replaced by `<not-found>` string literal.
+        """Gets requested decorated function's scope variables, specified in
+        `pass_vars` attribute. Any missing requested scope variables will be
+        replaced by `<not-found>` string literal.
 
         Args:
             N/A
         Returns:
-            dict: Names and values map of requested decorated function's scope variables.
+            dict: Names and values map of requested decorated function's
+                scope variables.
         Raises:
             N/A
         """
@@ -115,7 +117,10 @@ class ExtendedCallBackMixin:
         if not self.pass_vars:
             return {}
         for var_name in self.pass_vars:
-            func_scope_vars[var_name] = self.local_vars.get(var_name, "<not-found>")
+            func_scope_vars[var_name] = self.local_vars.get(
+                var_name,
+                "<not-found>",
+            )
         self.local_vars = {}
         return func_scope_vars
 
@@ -132,8 +137,8 @@ class ExtendedCallBackMixin:
         sys.setprofile(tracer)
 
     def tracer(self, frame, event, _):
-        """Represents tracer for storing local variables from last executed function.
-        Upon function return, this tracer saves function locals to `local_vars` instance attribute.
+        """Represents tracer storing local variables from executed function.
+        Upon function return, tracer saves function locals to `local_vars`.
 
         Args:
             frame (Frame): Frame to be traced.
