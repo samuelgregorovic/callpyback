30d07b92e54281b28e127de1126c880d48a5c25f
samuelgreorovic <samuelgregorovic@gmail.com>
2023-02-07 20:02:55 +0100

    release 1.2.1 to pypi

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ LONG_DESCRIPTION = (CUR_DIR / "README.md").read_text()
 setup_args = dict(
     name="callpyback",
     packages=find_packages(),
-    version="v1.2.0",
+    version="v1.2.1",
     description="Simple and readable Pure Python callbacks!",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     author="samuelgregorovic",
     author_email="samuelgregorovic@gmail.com",
     url="https://github.com/samuelgregorovic/callpyback",
-    download_url="https://github.com/samuelgregorovic/callpyback/archive/refs/tags/v.1.2.0.tar.gz",
+    download_url="https://github.com/samuelgregorovic/callpyback/archive/refs/tags/v.1.2.1.tar.gz",
     keywords=["callpyback", "callback", "python", "pure", "pythonic", "background"],
     classifiers=[],
 )
